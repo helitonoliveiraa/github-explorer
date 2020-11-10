7346c9a24ec833c5bf8e59f40fe32cfae32825e9
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-11-10 11:45:55 -0300

    :art: layout styling Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => (
-  <Title>Explore Repositórios no Github.</Title>
+  <>
+    <img src={logoImg} alt="Logo Github Explore" />
+    <Title>Explore Repositórios no Github.</Title>
+
+    <Form>
+      <input type="text" placeholder="Digite o nome do repositório" />
+      <button type="button">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars3.githubusercontent.com/u/45343619?s=460&u=1929279b8eb8dbe2434c20e7ad7e239674b4cc17&v=4"
+          alt="heliton"
+        />
+
+        <div>
+          <strong>helitonoliveira/repo</strong>
+          <p>descrição do repositório</p>
+        </div>
+
+        <FiChevronRight size={20} color="#C9C9D4" />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars3.githubusercontent.com/u/45343619?s=460&u=1929279b8eb8dbe2434c20e7ad7e239674b4cc17&v=4"
+          alt="heliton"
+        />
+
+        <div>
+          <strong>helitonoliveira/repo</strong>
+          <p>descrição do repositório</p>
+        </div>
+
+        <FiChevronRight size={20} color="#C9C9D4" />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars3.githubusercontent.com/u/45343619?s=460&u=1929279b8eb8dbe2434c20e7ad7e239674b4cc17&v=4"
+          alt="heliton"
+        />
+
+        <div>
+          <strong>helitonoliveira/repo</strong>
+          <p>descrição do repositório</p>
+        </div>
+
+        <FiChevronRight size={20} color="#C9C9D4" />
+      </a>
+    </Repositories>
+  </>
 );
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,96 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+  max-width: 450px;
+  margin-top: 100px;
+  line-height: 56px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 714px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 72px;
+    border: none;
+    padding: 0px 30px;
+    border-radius: 5px 0 0 5px;
+    font-size: 20px;
+    color: #3a3a3a;
+
+    ::placeholder {
+      color: #a8a8b3;
+      font-size: 20px;
+    }
+  }
+
+  button {
+    width: 210px;
+    border: none;
+    border-radius: 0 5px 5px 0;
+    background: #04d361;
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.ul`
+  margin-top: 120px;
+  max-width: 714px;
+
+  a {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 112px;
+    padding: 16px;
+    border-radius: 5px;
+    background: #fff;
+    text-decoration: none;
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 24px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+    }
+  }
 `;
